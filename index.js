58ac0b35402e22a8c1c062b56ff579feca8af697
Panza <gabrielpanzeri@live.com>
2020-09-09 10:25:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// precisa-se usar os espaços nos lugares corretos
+
+// se for usar apenas uma vez, utilizar o id. se houver possibilidade de reuso, utilizar class 
+
+let userController = new UserController("form-user-create", "table-users")
+
+
+
+
+
+
